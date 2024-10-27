78ff814f1813b7fcf14e9e4fc8dbef94300b2553
maradC <maradiagachristopher5@gmail.com>
2024-10-27 13:14:07 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.regex_hw_311 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.regex_hw_311 to javafx.fxml;
+    exports org.example.regex_hw_311;
+}
